219f57f7b542e1162ac293016a223887b61706f3
Ildiko Czeller <czeildi@gmail.com>
2018-06-16 19:57:06 +0200

    add reprex

---
+++ reprex.R
@@ -0,0 +1,11 @@
+datasetsexperiment::print_internal_in_body()
+datasetsexperiment::print_internal_from_argument()
+datasetsexperiment::print_external_in_body()
+datasetsexperiment::print_external_from_argument()
+
+library("datasetsexperiment")
+
+datasetsexperiment::print_internal_in_body()
+datasetsexperiment::print_internal_from_argument()
+datasetsexperiment::print_external_in_body()
+datasetsexperiment::print_external_from_argument()
